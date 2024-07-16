0408bc787199dc488f1f1518dd689aa0a8fc999c
Meghasyam <meghasyam283@gmail.com>
2024-07-16 18:24:18 +0530

    added code for clearing arrays

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const images = [];
 const values = [];
 
 function rollDice() {
+  const images = [];
+  const values = [];
   for (let i = 0; i < numOfDice.value; i++) {
     const value = Math.floor(Math.random() * 6 + 1);
     values.push(value);
